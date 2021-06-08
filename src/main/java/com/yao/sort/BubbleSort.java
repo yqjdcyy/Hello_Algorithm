4ea105a9a4ec43c40e758f4ea46b482ffe49f2c1
好久不见 <yqjdcyy@gmail.com>
2021-06-08 22:42:06 +0800

    ignore*

--- src/main/java/com/yao/sort/BubbleSort.java
+++ src/main/java/com/yao/sort/BubbleSort.java
@@ -23,9 +23,9 @@ public class BubbleSort {
         for (int i = 0; i < length - 1; i++) {
             for (int j = 0; j < length - 1 - i; j++) {
                 if (array[j] > array[j + 1]) {
-                    final Integer tmp = array[j + 1];
-                    array[j + 1] = array[j];
-                    array[j] = tmp;
+                    Integer tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
                 }
             }
         }
